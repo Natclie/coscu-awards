d32e09dea0add7fdba917280221c2174380883e2
Natclie <juanrudas2004@gmail.com>
2025-02-27 23:54:06 -0500

    fixed nav mobile

--- src/script.js
+++ src/script.js
@@ -1,7 +1,7 @@
 import AOS from 'aos';
-    AOS.init({
-        duration: 500,
-    });
+AOS.init({
+    duration: 500,
+});
 
 // Mobile navigation
 const navMobile = document.getElementById("navMobile");
@@ -9,14 +9,12 @@ const navMobile = document.getElementById("navMobile");
 function openNav() {
     if (navMobile) {
         navMobile.style.display = 'block';
-        document.body.style.overflow = 'hidden';
     }
 }
 
 function closeNav() {
     if (navMobile) {
         navMobile.style.display = 'none';
-        document.body.style.overflow = '';
     }
 }
 
